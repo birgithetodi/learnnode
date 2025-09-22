9b23dd91ef6c0a3dfab079e63e4eb284b452925b
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-09-22 15:10:18 +0300

    Add type examples

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-var foo;
-let bar;
-console.log(foo);
-console.log(bar);
+let num = 1;
+console.log(num);
+console.log(typeof num);
+num = 1.2;
+console.log(num);
+console.log(typeof num);
 
-var foo = 'Hello'; //old way dont use! hoisting
-let bar = 'World'; //muutuja
+let text = 'Hello world!';
+console.log(text);
+console.log(typeof text);
+text = 'H'; //char
+console.log(text);
+console.log(typeof text);
+
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+let nil = null; //väärtus puudub
+console.log(nil);
+console.log(typeof nil);
+
+
+let array = [1, 2, 'Hello world', true, [1, 2, 3]]; //array on massiiv
+console.log(array);
+console.log(typeof array);
+
+let obj = {
+    name: 'Birgithe',
+    age: '17',
+    hairColor: 'Light brown with pink/blonde Highlight',
+    isWoman: true,
+}
+console.log(obj);
+console.log(typeof obj);
