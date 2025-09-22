4bb1168bf2ed119a221347f8d71bda651a39e322
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-09-22 15:52:17 +0300

    Add math examples

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
+let answer = 1 + 1;
+answer = 1 - 1;
+answer = 2 * 3;
+answer = 5 / 5;
+answer = 7 % 2;
+answer = 2 ** 3; //astendamine
+console.log(answer);
+
 let num = 1;
+num = num + 1;
+num += 1;
+num -= 1;
+num *= 2;
+num /= 3;
+num %= 2;
+num **= 2;
+num++;
+num--;
+num = 1;
+Math.pow(3, 4);
+num = Math.pow(3, 4);
+num = 0.1 + 0.2;
 console.log(num);
-console.log(typeof num);
-num = 1.2;
-console.log(num);
-console.log(typeof num);
-
-let text = 'Hello world!';
-console.log(text);
-console.log(typeof text);
-text = 'H'; //char
-console.log(text);
-console.log(typeof text);
-
-let bool = true;
-console.log(bool);
-console.log(typeof bool);
-bool = false;
-console.log(bool);
-console.log(typeof bool);
-
-let undef = undefined;
-console.log(undef);
-console.log(typeof undef);
-
-let nil = null; //väärtus puudub
-console.log(nil);
-console.log(typeof nil);
-
-
-let array = [1, 2, 'Hello world', true, [1, 2, 3]]; //array on massiiv
-console.log(array);
-console.log(typeof array);
-
-let obj = {
-    name: 'Birgithe',
-    age: '17',
-    hairColor: 'Light brown with pink/blonde Highlight',
-    isWoman: true,
-}
-console.log(obj);
-console.log(typeof obj);
