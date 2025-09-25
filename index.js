b6967c65b75427d0a1078977332c7afbd389011a
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-09-25 13:35:55 +0300

    Add string methods examples

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-let answer = 1 + 1;
-answer = 1 - 1;
-answer = 2 * 3;
-answer = 5 / 5;
-answer = 7 % 2;
-answer = 2 ** 3; //astendamine
-console.log(answer);
+let text = 'Hello ';
+text = text + 'Biku!';
+console.log(text);
+text = "douscb"; //topeltjutumärgid kui ' tahad teksti sees kasutada
+text = 'edcewncue';
 
-let num = 1;
-num = num + 1;
-num += 1;
-num -= 1;
-num *= 2;
-num /= 3;
-num %= 2;
-num **= 2;
-num++;
-num--;
-num = 1;
-Math.pow(3, 4);
-num = Math.pow(3, 4);
-num = 0.1 + 0.2;
-console.log(num);
+text = "Don't use double quotes unless needed";
+text = 'Don\'t use double quotes\n unless needed'; //kaldkriipsuga saab kirjutada don't-ile ' ülakoma ilma et see errori annaks
+// \n annab uue rea. uue rea sümbol (new line)
+console.log(text);
+
+let name = 'Birgithe';
+let age = 17;
+let sexuality = 'Gay';
+text = 'You\'r name is ' + name + ' and you are ' + age + ' years old and you\'re sexuality is ' + sexuality + '!';
+console.log(text);
+
+text = `Your name is ${name} 
+and you are ${age} years old 
+and you are ${sexuality}!`;
+// `` nendega saad panna teksti mitmele reale nii et see ei läheks katki "" ja '' märkidega lähb katki
+console.log(text);
+
+text = text.toUpperCase();
+console.log(text);
