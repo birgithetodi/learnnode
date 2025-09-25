5ec5c13b7a3139410440aad9bfd178c53d0ac438
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-09-25 16:59:33 +0300

    Add korduslaused examples

--- index.js
+++ index.js
@@ -1,74 +1,33 @@
-let num = 10;
-if(num > 10) {
-    console.log('Bigger than 10');
-} 
-else if (num === 10){
-    console.log('Equal with 10');
-}
-else {
-    console.log('Smaller than 10');
+for(let i=0; i<10; i++) {
+    console.log(i);
 }
 
-let day = new Date("2025-09-25").getDay(); //eesti kuupäeva kirjutades 25/09/2025
-console.log(day);
+let sum = 0;
+for(let i=0; i<1_000_000; i++) {
+    sum += i;
+}
+console.log(sum)
 
-if(day === 1) {
-    console.log('Esmaspäev');
-} else if (day === 2){
-    console.log('Teisipäev');
-} else if (day === 3){
-    console.log('Kolmapäev');
-} else if (day === 4){
-    console.log('Neljapäev');
-} else if (day === 5){
-    console.log('Reede');
-} else if (day === 6){
-    console.log('Laupäev');
-} else if (day === 0){
-    console.log('Pühapäev');
-} else {
-    console.log('Imelik päev');
-};
-
-switch(day) {
-    case 1:
-        console.log('Esmaspäev');
-        break;
-    case 2:
-        console.log('Teisipäev');
-        break;
-    case 3:
-        console.log('Kolmapäev');
-        break;
-    case 4:
-        console.log('Neljapäev');
-        break;
-    case 5:
-        console.log('Reede');
-        break;
-    case 6:
-        console.log('Laupäev');
-        break;
-    case 0:
-        console.log('Pühapäev');
-        break;
-    default:
-        console.log('Imelikud päevad');
+for(let i = 10; i>=0; i--) {
+    console.log(i);
 }
 
-//else if(day === 5 || day === 6) {console.log('Pidupäev')}
-//selle koodiga saab mitmele asjale sama kirjelduse panna
+for(let i=1; i<1_000_000; i*=2) {
+    console.log(i);
+}
 
-//case 5: case 6: console.log('Laupäev'); break;
-//selle koodiga saab sama teha
-
-let days = ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev'];
-console.log(days[day]);
+let time = Date.now();
+let num = 0;
+while(Date.now() < time+1000) {
+    num++;
+}
+console.log(num);
 
 num = 10;
-answer = num > 10 ? 'Suurem' : 'Väiksem'; //shorthand if
-console.log(answer);
+while(num < 10) {
+    console.log('while');
+}
 
-let value = 'HAHA'; //null coalecing operator
-answer = value ?? 'Something else';
-console.log(answer);
+do {
+    console.log('DO');
+} while(num < 10);
