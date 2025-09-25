0485d775065dea09c451c61ec44131167147796d
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-09-25 14:50:32 +0300

    Add function examples

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
-let obj = {
-    name: 'Biku',
-    age: 17,
-    sexuality: 'Gay',
-    fruits: ['pineapple', 'blueberry', 'strawberry'],
-    cat: {
-        age: 3,
-        name: 'Kitty',
-        gender: 'female',
+function hello() {
+    console.log('Hello world!')
+}
+
+hello();
+hello();
+hello();
+hello();
+
+function helloName(name) {
+    console.log(`Hello ${name}!`)
+}
+
+helloName('Birgithe');
+helloName('Marily');
+
+function hi(name='Nameless', age='infinite') {
+    console.log(`Hi ${name} you are ${age} years old`)
+}
+
+hi('Birgithe', 17);
+hi('Marily', 16);
+hi();
+
+let sum = function (a, b) {
+    return a+b;
+}
+
+let answer = sum(4, 5);
+console.log(answer);
+console.log(sum(6, 10));
+sum(5, 6);
+
+let sub = (a, b) => {
+    return a-b;
+}
+console.log(sub(6, 10));
+
+let mult = (a, b) => a*b;
+console.log(mult(6, 10))
+
+let root = a => a*a;
+console.log(root(6, 10));
+
+let cool = () => console.log('cool');
+cool();
+
+let awesome = {
+    party: function () {
+        console.log('PARTY!');
     },
-    parentName: 'Monika',
-    as2sd: 'asd',
-    'parent-name': 'name',
-    'as2sd': 'asd',
-    'some things': 'asd2',
+    cool: () => console.log('cool'),
+    nice() {
+        console.log('nice');
+    }
 };
-
-console.log(obj); //annab kogu info
-console.log(obj.name); //obj. ... annab teatud info
-console.log(obj.fruits[2]); //annab fruits seest teise sõna mis on
-console.log(obj.cat.name); //annab objekti seest ainult1 teatud asja
-console.log(obj['parent-name']);
-console.log(Object.keys(obj)); //teeb array kus on aiult võtmete nimed (name,sexuality,cat,age jne)
-
-//kui panna see parentName siis see läheb errorisse kui panna "parent name", "parent-name"jne
-//2asd 'asd', not allowed to start with a number
-// read 13-15 pol soovitatav kuna tekivad vead
-//console.log(obj.parent-name) annab errori kuna ta arvab et sa tahad lahutada parent nimest, rida 22 on õige varjant
+awesome.party();
+awesome.cool();
+awesome.nice();
