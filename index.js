5edb2fb8e2898900e3317ef184673a794fe0a28a
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-09-25 16:11:08 +0300

    Add if and switch examples

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
-let bool = true;
-console.log(bool);
-bool = false;
-console.log(bool);
+let num = 10;
+if(num > 10) {
+    console.log('Bigger than 10');
+} 
+else if (num === 10){
+    console.log('Equal with 10');
+}
+else {
+    console.log('Smaller than 10');
+}
 
-bool = true &&  false; //ainuke võimalus kus vastus tuleks true peab see olema true && true
-bool = true &&  true; //vastus true
-bool = false &&  false; //vastus false
-bool = false && true; //vastus false
-console.log(bool);
+let day = new Date("2025-09-25").getDay(); //eesti kuupäeva kirjutades 25/09/2025
+console.log(day);
 
-bool = true || false; //OR, vastus on true
-bool = false || true; //vastus on true
-bool = false || false; //vastus on false
-bool = true || true; //vastus on true
-console.log(bool);
+if(day === 1) {
+    console.log('Esmaspäev');
+} else if (day === 2){
+    console.log('Teisipäev');
+} else if (day === 3){
+    console.log('Kolmapäev');
+} else if (day === 4){
+    console.log('Neljapäev');
+} else if (day === 5){
+    console.log('Reede');
+} else if (day === 6){
+    console.log('Laupäev');
+} else if (day === 0){
+    console.log('Pühapäev');
+} else {
+    console.log('Imelik päev');
+};
 
-bool = !true; //NOT tehe(pole true)
-console.log(bool);
-bool = !false;
-console.log(bool);
+switch(day) {
+    case 1:
+        console.log('Esmaspäev');
+        break;
+    case 2:
+        console.log('Teisipäev');
+        break;
+    case 3:
+        console.log('Kolmapäev');
+        break;
+    case 4:
+        console.log('Neljapäev');
+        break;
+    case 5:
+        console.log('Reede');
+        break;
+    case 6:
+        console.log('Laupäev');
+        break;
+    case 0:
+        console.log('Pühapäev');
+        break;
+    default:
+        console.log('Imelikud päevad');
+}
 
-bool = (true && false || true) && !(false || true && false);
-console.log(bool);
+//else if(day === 5 || day === 6) {console.log('Pidupäev')}
+//selle koodiga saab mitmele asjale sama kirjelduse panna
 
-bool = 10 > 5;
-bool = 10 < 5;
-bool = 10 > 10;
-bool = 10 < 10;
-bool = 10 == 10;
-bool = '10' == 10; //true kuna see teeb '10' tavaliseks kümneks ja see on võrdne 10
-bool = '10' === 10; //vale "range võrdus" kõik peab klappima
-bool = 10 >= 10;
-bool = 10 <= 10;
-bool = '10' != 10; //!= on mittevõrsus
-bool = '10' !== 10; //range mitte võrdne tehe
-console.log(bool);
-
-bool = [] == ''; //true
-bool = [1, 2, 3] == 1; //false
-console.log(bool);
-
-
+//case 5: case 6: console.log('Laupäev'); break;
+//selle koodiga saab sama teha
