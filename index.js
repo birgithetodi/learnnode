1255ba36f9428b593f2b67a0b8f2f676ab6ebaf3
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-09-29 14:58:12 +0300

    Add array and object examples

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
-for(let i=0; i<10; i++) {
-    console.log(i);
+let array = ['apple', 'pineapple', 'blueberry', 'strawberry'];
+
+for(fruit of array) {
+    console.log(fruit);
 }
 
-let sum = 0;
-for(let i=0; i<1_000_000; i++) {
-    sum += i;
-}
-console.log(sum)
+array.forEach(fruit =>{
+    console.log(fruit);
+});
 
-for(let i = 10; i>=0; i--) {
-    console.log(i);
+let obj = {
+    name: 'Birgihe',
+    age: 17,
+    color: 'Pink',
 }
+for(key in obj) {
+    console.log(key);
+    console.log(obj[key]);
+}; 
+//obj[key] annab väärtuse ka
+//annab võtme ehk name,age ja color
 
-for(let i=1; i<1_000_000; i*=2) {
-    console.log(i);
-}
+let capital = array.map(fruit => {
+    return fruit.toUpperCase();
+});
+console.log(capital);
 
-let time = Date.now();
-let num = 0;
-while(Date.now() < time+1000) {
-    num++;
-}
-console.log(num);
+let aFruits = array.filter(fruit => {
+    return fruit.includes('a');
+});
+console.log(aFruits)
 
-num = 10;
-while(num < 10) {
-    console.log('while');
-}
-
-do {
-    console.log('DO');
-} while(num < 10);
-
-function recursion(num) {
-    if(num<10){
-        console.log(num);
-        recursion(num+1);
-    }
-}
-recursion(0);
+let nums = [1, 2, 3, 4, 5, 6, 7];
+let sum = nums.reduce((total, num) => {
+    return total + num;
+});
+console.log(sum);
