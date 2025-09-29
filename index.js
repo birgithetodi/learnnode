df484c640c44d3543aff60729a46c915069fd2c6
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-09-29 15:37:20 +0300

    everything is object example

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-let array = ['apple', 'pineapple', 'blueberry', 'strawberry'];
+let  text = 'Hello world';
+console.log(text[3]);
 
-for(fruit of array) {
-    console.log(fruit);
-}
-
-array.forEach(fruit =>{
-    console.log(fruit);
-});
-
-let obj = {
-    name: 'Birgihe',
-    age: 17,
-    color: 'Pink',
-}
-for(key in obj) {
-    console.log(key);
-    console.log(obj[key]);
-}; 
-//obj[key] annab väärtuse ka
-//annab võtme ehk name,age ja color
-
-let capital = array.map(fruit => {
-    return fruit.toUpperCase();
-});
-console.log(capital);
-
-let aFruits = array.filter(fruit => {
-    return fruit.includes('a');
-});
-console.log(aFruits)
-
-let nums = [1, 2, 3, 4, 5, 6, 7];
-let sum = nums.reduce((total, num) => {
-    return total + num;
-});
-console.log(sum);
+let array = [1, 2, 3];
+let num = 2;
+num.toExponential();
+array.fill(1,3,6);
+console.log(text.length);
+console.log(Math.PI);
