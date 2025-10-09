eb61c5a40989e0da7eb74e6a22073db65d44a769
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-10-09 16:53:23 +0300

    Add simple item list example with VUE

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+    let newItem = ref('Hello Vue!');
+    let items = ref(['Aperol', 'Apelsin', 'Schweppes']);
+    let code = ref(404);
+
+function addItem(){
+    items.value.push(newItem.value);
+    newItem.value = '';
+}
+
+</script>pt
+<template>
+<div class="container">
+    <div class="content">
+        <div class="field has-addons">
+            <div class="control">
+                <input v-model="newItem" class="input" type="text" placeholder="Add item">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add item
+                </button>
+            </div>
+        </div>
+        <h1>{{ newItem }}</h1>
+        <ul>
+            <li v-for="item in items">{{ item }}</li>
+        </ul>
+        <input v-model="code" class="input" type="number" placeholder="Enter error code">
+        <img :src="'https://http.cat/'+ code">
+    </div>
+</div>
+</template>
+
+//v-bind:value="message" v-on:input="message=$event.target.value" on sama mis v-model aga raskem
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import './style.scss';
 
-let input = document.querySelector('input#new-item');
-let button = document.querySelector('#add-btn');
-let list = document.querySelector('#shopping-list');
-button.addEventListener('click', () => {
-    let newItem = input.value;
-    list.innerHTNL += `<li>${newItem}</li>`;
-    input.value = '';
-});
+import { createApp } from 'vue';
 
-let title =document.querySelector('#title');
-input.addEventListener('input', () => {
-    title.innerHTML = input.value;
-});
+import App from './App.vue';
+
+let app = createApp(App);
+app.mount('#app');
