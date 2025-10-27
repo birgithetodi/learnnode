48273e3b9c33d6f3374f53b88aa496ebee71c52f
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-10-27 15:17:08 +0200

    Add infinite scroll excample

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -21,36 +21,11 @@ async function getCharacters(page) {
         }
     });
     console.log(res);
-    characters.value = res.data.results;
+    characters.value.push(...res.data.results);
     info.value = res.data.info;
 }
 
 await getCharacters(currentPage.value);
-
-function generatePages(count, current) {
-    let pages = [];
-    for (let i = 1; i <= 3; i++) {
-        pages[i] = i;
-    }
-    if (current > 2) {
-        pages.push('...');
-    }
-    if (current > 2 && current < count - 1) {
-
-
-        for (let i = current - 2; i <= current + 2; i++) {
-            pages[i] = i;
-        }
-    }
-    if (current < count - 2) {
-        pages.push('...');
-    }
-    for (let i = count - 2; i <= count; i++) {
-        pages[i] = i;
-    }
-    return pages.filter(page => page);
-}
-console.log(generatePages(42, 10));
 
 let timeout;
 
@@ -61,6 +36,15 @@ async function getPage(page) {
     await getCharacters(currentPage.value);
   }, 1000);
 }
+document.addEventListener('scroll', function() {
+    console.log(window.innerHeight + window.scrollY, document.body.clientHeight);
+    if(window.innerHeight + window.scrollY > document.body.clientHeight - 100) {
+        getPage(currentPage.value + 1);
+    }
+}); 
+
+
+
 
 </script>
 <template>
@@ -72,24 +56,7 @@ async function getPage(page) {
             <button class="button is-info" @click="getPage(1)">Search</button>
         </div>
     </div>
-    <nav class="pagination" role="navigation" aria-label="pagination">
-        <a href="#" class="pagination-previous" :disabled="!info.prev" @click="getPage(currentPage - 1)">Previous</a>
-        <a href="#" class="pagination-next" :disabled="!info.next" @click="getPage(currentPage + 1)">Next page</a>
-        <ul class="pagination-list">
-
-            <li v-for="page in generatePages(info.pages, currentPage)">
-                <a v-if="page === currentPage" class="pagination-link is-current" aria-label="Page 46"
-                    aria-current="page">{{ page }}
-                </a>
-                <a v-else-if="page !== '...'" href="#" class="pagination-link" @click="getPage(page)"
-                    aria-label="Goto page 1">{{ page }}
-                </a>
-                <span v-else class="pagination-ellipsis">&hellip;
-
-                </span>
-            </li>
-        </ul>
-    </nav>
+    
     <div class="columns is-multiline">
         <div class="column is-3" v-for="character in characters">
             <CharacterCard :character="character"></CharacterCard>
