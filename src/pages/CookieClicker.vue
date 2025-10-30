2aca5c82d839740b5b4cad4a72e8093a7f8a048c
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-10-30 14:18:49 +0200

    finish cookie clicker

--- src/pages/CookieClicker.vue
+++ src/pages/CookieClicker.vue
@@ -1,22 +1,44 @@
 <script setup>
 import { ref } from 'vue';
+import BuildingButton from '../components/BuildingButton.vue';
 
-    let cookies = ref(0);
-    
+let cookies = ref(0);
+let cps = ref(0);
+
+setInterval(() => {
+    cookies.value += cps.value;
+}, 1000);
+
+let buildings = ref([
+    { name: 'Cursor', icon: '☝️', cps: 0.1, price: 15, count: 0 },
+    { name: 'Grandma', icon: '👵🏽', cps: 1, price: 100, count: 0 },
+])
+
+function buyBuilding(building) {
+    if (cookies.value >= building.price) {
+        cookies.value -= building.price;
+        cps.value += building.cps;
+        building.count++;
+    }
+}
+
 </script>
 <template>
     <div class="columns">
         <div class="column has-background-danger has-text-centered">
-            <h1>{{ cookies }} cookies!</h1>
+            <h1>{{ parseFloat(cookies.toFixed(1)) }} cookies!</h1>
+             <h3>per second {{ parseFloat(cps.toFixed(1)) }}</h3>
             <div @click="cookies++">
-            <img src="https://www.pikpng.com/pngl/b/32-323639_chocolate-cookies-png-cookie-clipart.png" alt="">
+                <img src="https://www.pikpng.com/pngl/b/32-323639_chocolate-cookies-png-cookie-clipart.png" alt="">
             </div>
-    </div>
-        <div class="column has-background-warning">
+        </div>
+        <div class="column has-background-link">
             test
         </div>
         <div class="column has-background-primary">
-            test
+            <BuildingButton v-for="building in buildings" :building="building" :disabled="cookies < building.price"
+                @click="buyBuilding(building)"></BuildingButton>
         </div>
     </div>
+
 </template>
