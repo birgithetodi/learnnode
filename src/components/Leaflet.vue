87b60dd8efe8a360a7d0ff530285e9d9383a7a1c
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-11-06 19:49:37 +0200

    Add go home, circle and polygon markers

--- src/components/Leaflet.vue
+++ src/components/Leaflet.vue
@@ -5,9 +5,9 @@ import 'leaflet/dist/images/marker-icon-2x.png';
 import 'leaflet/dist/images/marker-shadow.png';
 
 import { onMounted, useId, watch } from 'vue';
-let {lat, lng, zoom} = defineProps(['lat', 'lng', 'zoom']);
+let { lat, lng, zoom } = defineProps(['lat', 'lng', 'zoom']);
 
-let id='map-' + useId();
+let id = 'map-' + useId();
 let map = null;
 onMounted(() => {
     map = L.map(id).setView([lat, lng], zoom);
@@ -16,6 +16,20 @@ onMounted(() => {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
     var marker = L.marker([59.42681, 24.743445]).addTo(map);
+
+    var polygon = L.polygon([
+        [59.21672, 24.65902],
+        [59.21671, 24.65931],
+        [59.21661, 24.65931],
+        [59.21661, 24.65901],
+    ]).addTo(map);
+
+    var circle = L.circle([59.21676, 24.65916], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 5
+}).addTo(map);
 });
 
 watch(() => zoom, (zoom, oldZoom) => {
@@ -31,9 +45,11 @@ watch(() => lng, lng => {
 
 </script>
 <template>
-    <div :id="id" ></div>
+    <div :id="id"></div>
 </template>
 
 <style scoped>
-div { height: 40vh; }
+div {
+    height: 40vh;
+}
 </style>
--- src/pages/LeafletMap.vue
+++ src/pages/LeafletMap.vue
@@ -14,6 +14,7 @@ let coords = ref({
         <input v-model="zoom" type="range" max="19" min="1" step="1">
         {{ zoom }}
         <button class="button is-primary" @click="coords={lat: 59.41632, lng: 24.79890}">Go to Airport</button>
+        <button class="button is-link" @click="coords={lat: 59.21675, lng: 24.65914}">Go Home</button>
     </div>
     <Leaflet :lat="coords.lat" :lng="coords.lng" :zoom="zoom"></Leaflet>
     <Leaflet :lat="24.743445" :lng="59.42681" :zoom="5"></Leaflet>
