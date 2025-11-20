371299ab5099c76c825b84522055794945e50b62
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-11-20 15:48:49 +0200

    Add inquerer

--- bin/terminal.js
+++ bin/terminal.js
@@ -7,3 +7,21 @@ import chalk from "chalk";
 write(chalk.red.bgYellow.strikethrough('Hello Biku'));
 write(chalk.bgAnsi256(67)('67!'));
 write(chalk.bgRgb(255, 28, 98)('HAYY <3'));
+
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([
+{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+},
+{
+    type: 'input',
+    name: 'age',
+    message: 'How old ar you?',
+},
+]);
+
+console.log(`Hello ${answers.name}!!!!!`)
+console.log(`You are ${answers.age} years old!`)
