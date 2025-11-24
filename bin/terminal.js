76f95fd760ce3360c3b924a317957654371c88fe
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-11-24 15:04:40 +0200

    Add small game excample

--- bin/terminal.js
+++ bin/terminal.js
@@ -2,11 +2,37 @@ function write(text) {
   process.stdout.write(text);
 }
 
-let date = new Date().toISOString().slice(11, 19);
 
- write(date);
-setInterval(() => {
-  date = new Date().toISOString().slice(11, 19);
-    write('\x1b[8D')
-    write(date);
-}, 1000);
+import readline from "readline";
+const blank = '\n'.repeat(process.stdout.rows)
+console.log(blank)
+readline.cursorTo(process.stdout, 0, 0)
+readline.clearScreenDown(process.stdout)
+
+write('@');
+
+import { read } from "fs";
+import keypress from "keypress";
+
+keypress(process.stdin);
+
+process.stdin.on('keypress', (ch, key) => {
+    //console.log(ch, key);
+    if(key && key.name === 'c' && key.ctrl) {
+        process.exit();
+    }
+    if(key.name === 'd'){
+        write('\x1b[1D');
+        write(' ');
+        write('@');
+    }
+    if(key.name === 'a'){
+        write('\x1b[2D');
+        write('@');
+        write(' ');
+        write('\x1b[1D');
+    }
+});
+  
+
+process.stdin.setRawMode(true);
