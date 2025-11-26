b638363341160825ef72ac4c18005d09c9223d5c
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-11-26 17:22:44 +0200

    Add rawlist ja checkbox examples

--- bin/questions.js
+++ bin/questions.js
@@ -1,27 +1,37 @@
-function write(text) {
-    process.stdout.write(text);
-};
-
-import chalk from "chalk";
-
-write(chalk.red.bgYellow.strikethrough('Hello Biku'));
-write(chalk.bgAnsi256(67)('67!'));
-write(chalk.bgRgb(255, 28, 98)('HAYY <3'));
-
-import inquirer from "inquirer";
+import inquirer from 'inquirer';
 
 let answers = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'action',
+        message: 'Would you like to order a pizza? (Please type "order" to continue)',
+    },
+    {
+        type: 'rawlist',
+        name: 'size',
+        message: 'What size do you need?',
+        choices: [
+            { name: 'Jumbo', value: 'jumbo' },
+            { name: 'Large', value: 'large' },
+            { name: 'Standard', value: 'standard' },
+            { name: 'Medium', value: 'medium' },
+            { name: 'Small', value: 'small' },
+            { name: 'Micro', value: 'micro' },
+        ],
+when: (answers) => answers.action === 'order',
+    },
 {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?',
-},
-{
-    type: 'input',
-    name: 'age',
-    message: 'How old ar you?',
-},
+    type: 'checkbox',
+        name: 'topping',
+            message: 'What extra toppings do you want?',
+                choices: [
+                    { name: 'cheese', value: 'Cheese' },
+                    { name: 'pepperoni', value: 'Pepperoni' },
+                    { name: 'onion', value: 'Onion' },
+                    { name: 'mushrooms', value: 'Mushrooms' },
+                    { name: 'pineapple', value: 'Pineapple' },
+                ],
+    },
 ]);
 
-console.log(`Hello ${answers.name}!!!!!`)
-console.log(`You are ${answers.age} years old!`)
+console.log('Order has submitted!');
