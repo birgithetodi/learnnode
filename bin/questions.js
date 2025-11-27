a6fd6d3ee47319f008b5471b89b3c16bc6c1b2d4
Birgithe Todi <birgithe.todi@tptlive.ee>
2025-11-27 14:34:44 +0200

    Add inquirer list excamples

--- bin/questions.js
+++ bin/questions.js
@@ -1,37 +1,20 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 
-let answers = await inquirer.prompt([
-    {
-        type: 'input',
-        name: 'action',
-        message: 'Would you like to order a pizza? (Please type "order" to continue)',
-    },
-    {
-        type: 'rawlist',
-        name: 'size',
-        message: 'What size do you need?',
-        choices: [
-            { name: 'Jumbo', value: 'jumbo' },
-            { name: 'Large', value: 'large' },
-            { name: 'Standard', value: 'standard' },
-            { name: 'Medium', value: 'medium' },
-            { name: 'Small', value: 'small' },
-            { name: 'Micro', value: 'micro' },
-        ],
-when: (answers) => answers.action === 'order',
-    },
-{
-    type: 'checkbox',
-        name: 'topping',
-            message: 'What extra toppings do you want?',
-                choices: [
-                    { name: 'cheese', value: 'Cheese' },
-                    { name: 'pepperoni', value: 'Pepperoni' },
-                    { name: 'onion', value: 'Onion' },
-                    { name: 'mushrooms', value: 'Mushrooms' },
-                    { name: 'pineapple', value: 'Pineapple' },
-                ],
-    },
+const answers = await inquirer.prompt([
+    {type: 'input', name: 'sss', message: 'sdasda'},
+  {
+    type: "list",
+    name: "size",
+    message: "What size do you need?",
+    choices: [
+      { name: 'Jumbo', value: 'Jumbo' },
+      { name: 'Large', value: 'Large' },
+      { name: 'Standard', value: 'Standard' },
+      { name: 'Medium', value: 'Medium' },
+      { name: 'Small', value: 'Small' },
+      { name: 'Micro', value: 'Micro' },
+    ],
+  }
 ]);
 
-console.log('Order has submitted!');
+console.log(answers);
